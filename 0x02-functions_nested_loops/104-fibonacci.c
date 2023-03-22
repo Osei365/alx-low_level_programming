2048cee2b4c93bb222c82df2af1d6c446f4c95c4
Osei <williamsabodunrin@gmail.com>
2023-03-22 01:29:08 +0000

    Update 104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -6,9 +6,9 @@
  */
 int main(void)
 {
-	long n1 = 1;
-	long n2 = 2;
-	long n3;
+	long long int n1 = 1;
+	long long int n2 = 2;
+	long long int n3;
 	int i;
 
 	printf("%ld, ", n1);
@@ -18,11 +18,11 @@ int main(void)
 		n3 = n1 + n2;
 		if (i != 95)
 		{
-			printf("%ld, ", n3);
+			printf("%lld, ", n3);
 		}
 		else
 		{
-			printf("%ld\n", n3);
+			printf("%lld\n", n3);
 		}
 		n1 = n2;
 		n2 = n3;
