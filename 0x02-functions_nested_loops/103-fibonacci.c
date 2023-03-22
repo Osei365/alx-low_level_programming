dee1774fbbe71e9a337394ba1c88621b6c0c44ff
osei <williamsabodunrin@gmail.com>
2023-03-21 18:09:54 -0700

    advanced fibonacci

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -9,21 +9,18 @@ int main(void)
 	long sum = 0;
 	long n1 = 0;
 	long n2 = 1;
-	long n3 = 0;
+	long n3;
 
+	n3 = n1 + n2;
 	while (n3 <= 4000000)
 	{
-		n3 = n1 + n2;
 		if (n3 % 2 == 0)
 		{
 			sum = sum + n3;
 		}
-		else
-		{
-			continue;
-		}
 		n1 = n2;
 		n2 = n3;
+		n3 = n1 + n2;
 	}
 	printf("%ld\n", sum);
 	return (0);
