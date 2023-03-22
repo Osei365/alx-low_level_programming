3f93d731364b6600856ea1f85c4362c18f98776b
osei <williamsabodunrin@gmail.com>
2023-03-21 18:21:43 -0700

    more advanced fibonacci

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: always 0 (Success)
+ */
+int main(void)
+{
+	long n1 = 1;
+	long n2 = 2;
+	long n3;
+	int i;
+
+	printf("%ld, ", n1);
+	printf("%ld, ", n2);
+	for (i = 0; i < 96; i++)
+	{
+		n3 = n1 + n2;
+		if (i != 95)
+		{
+			printf("%ld, ", n3);
+		}
+		else
+		{
+			printf("%ld\n", n3);
+		}
+		n1 = n2;
+		n2 = n3;
+	}
+	return (0);
+}
+
