654cab27a31378f33d0d59eedbc4467d5e91f0f0
Osei <williamsabodunrin@gmail.com>
2023-03-23 15:24:36 +0000

    Update 5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -12,11 +12,11 @@ void more_numbers(void)
 	{
 		for (b = 0; b < 15; b++)
 		{
-			_putchar((b / 10) + '0');
 			if (b >= 10)
 			{
-				_putchar((b % 10) + '0');
+				_putchar((b / 10) + '0');
 			}
+			_putchar((b % 10) + '0');
 		}
 		_putchar('\n');
 	}
