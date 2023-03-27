f48fbc7229b600e25bf9621dc1f16b9d8fa22994
osei <williamsabodunrin@gmail.com>
2023-03-27 06:57:35 -0700

    length of string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include <string.h>
+#include "main.h"
+/**
+ * _strlen - returns length of string
+ * @s: string to be evaluated
+ * Return: length of string
+ */
+int _strlen(char *s)
+{
+	int l;
+	
+	l = strlen(s);
+	return (l);
+}
