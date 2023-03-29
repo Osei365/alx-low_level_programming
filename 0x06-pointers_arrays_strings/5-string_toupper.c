8798a722d0020383bb0ae66bf8d3b9c0bb3f03c2
osei <williamsabodunrin@gmail.com>
2023-03-29 04:44:27 -0700

    string to uppercase

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <string.h>
+/**
+ * string_toupper - converts lowercase to uppercase
+ * @s: string to be converted
+ * Return: char *
+ */
+char *string_toupper(char *s)
+{
+	int c;
+	int l, a;
+
+	l = strlen(s);
+	for (a = 0; a < l; a++)
+	{
+		c = *(s + a);
+		if (c >= 97 && c <= 122)
+		{
+			c = c - 32;
+		}
+		*(s + a) = c;
+	}
+	return (s);
+}
