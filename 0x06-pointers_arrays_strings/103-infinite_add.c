1512c3698d4767ba552b7cc4c33cbb150462024d
Osei <williamsabodunrin@gmail.com>
2023-03-30 09:25:10 +0000

    Update 103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -44,7 +44,7 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 	}
 	*(r + (size_r - 1)) = carr % 10 + '0';
 	size_r--;
-	if (size_r - 1 >= 0)
+	if (size_r >= 0)
 	return (r + (size_r));
 	else
 	return (0);
