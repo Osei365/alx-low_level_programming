4fb462561a4dc08f77f7947f5621b89c9009a7f5
osei <williamsabodunrin@gmail.com>
2023-04-03 15:40:27 -0700

    character location

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,11 @@
+#include <string.h>
+/**
+ * _strchr - locates a char in a string
+ * @s: pointer
+ * @c: char
+ * Return: char *
+ */
+char *_strchr(char *s, char c)
+{
+	return (strchr(s, c));
+}
