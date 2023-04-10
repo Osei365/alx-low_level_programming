dddf6e9245275033723565a011a77e49034a445a
osei <williamsabodunrin@gmail.com>
2023-04-10 07:44:11 -0700

    what is my name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * @argc: argument count
+ * @argv: argument array
+ * Return: always Success
+ */
+int main(int argc, char *argv[])
+{
+	if (argc >= 1)
+	printf("%s\n", argv[0]);
+	return (1);
+}
