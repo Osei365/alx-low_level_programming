84d05bec286ca89684e7891d48d3b5b08a2e7e74
Osei <williamsabodunrin@gmail.com>
2023-04-13 21:28:26 +0000

    string n concat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -22,7 +22,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (s2 == NULL)
 	s2 = "";
 
-	result = (char *)malloc((l1 + n + 1));
+	result = (char *)malloc(sizeof(char) * (l1 + n + 1));
 	if (!result)
 	return (NULL);
 	if (s1)
