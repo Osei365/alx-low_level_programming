ea8f6cdbe8739cfcffc16e95ccd12028cedabe43
osei <williamsabodunrin@gmail.com>
2023-04-13 16:47:45 -0700

    re allocation of memory

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <string.h>
+/**
+ * _realloc - re allocates memory
+ * @ptr: pointer
+ * @old_size: old size
+ * @new_size: new_size
+ * Return: void pointer
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	void *result = NULL;
+
+	if (new_size)
+	{
+		if (!ptr)
+		{
+			return (malloc(new_size));
+		}
+
+		result = malloc(new_size);
+		if (result)
+		{
+			if (old_size > new_size)
+			memcpy(result, ptr, new_size);
+			else if (new_size > old_size)
+			memcpy(result, ptr, old_size);
+			free(ptr);
+		}
+	}
+	return (result);
+}
