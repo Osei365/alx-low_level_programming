55cd9074e711fd4136f0391aa86b62d8b5abd70d
osei <williamsabodunrin@gmail.com>
2023-04-17 17:44:40 -0700

    new dog function

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,5 +1,6 @@
 #include "dog.h"
 #include <stdlib.h>
+#include <string.h>
 /**
  * new_dog - creates new dog in class dog_t
  * @name: name of dog
@@ -10,12 +11,34 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *result;
+	int l1, l2;
+
+	l1 = strlen(name);
+	l2 = strlen(owner);
 
 	result = malloc(sizeof(dog_t));
 	if (result == NULL)
 	return (NULL);
-	result->name = name;
+
+	result->name = malloc(sizeof(char) * (l1 + 1));
+	if (result->name != NULL)
+	strcpy(result->name, name);
+	else
+	{
+		free(result);
+		return (NULL);
+	}
+
 	result->age = age;
-	result->owner = owner;
+
+	result->owner = malloc(sizeof(char) * (l2 + 1));
+	if (result->owner != NULL)
+		strcpy(result->owner, owner);
+	else
+	{
+		free(result);
+		free(result->name);
+		return (NULL);
+	}
 	return (result);
 }
