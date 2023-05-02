6b9e92c06f71579fc054fe92b7a6256bae27e3df
osei <williamsabodunrin@gmail.com>
2023-05-02 05:08:26 -0700

    insert node

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -23,8 +23,9 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 			new->n = n;
 			new->next = NULL;
 			ptr->next = new;
+			ptr = new;
 			if (swap)
-				(new)->next = swap;
+				ptr->next = swap;
 			break;
 		}
 		ptr = ptr->next;
@@ -32,5 +33,5 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	}
 	if (a < idx)
 		return (NULL);
-	return (new);
+	return ptr;
 }
