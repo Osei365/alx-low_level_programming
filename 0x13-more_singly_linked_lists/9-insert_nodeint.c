4a295a821a659f35c4001ec83ac982a60c2a9804
Osei <williamsabodunrin@gmail.com>
2023-05-02 12:30:24 +0000

    insert node

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -13,7 +13,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	unsigned int a = 0;
 
 	new = malloc(sizeof(listint_t));
-	if (!new)
+	if (!new || !head)
 		return (NULL);
 	new->n = n;
 	new->next = NULL;
@@ -33,7 +33,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 			if (swap)
 				ptr->next = swap;
 			return (ptr);
-			break;
 		}
 		ptr = ptr->next;
 		a++;
