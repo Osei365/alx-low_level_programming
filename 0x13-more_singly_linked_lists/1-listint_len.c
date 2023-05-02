0e44c91497ed379bafe109ed057198c551695e46
osei <williamsabodunrin@gmail.com>
2023-05-01 18:09:44 -0700

    length of linked list

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * listsint_len - calcs length of h
+ * @h: linked list pointer
+ * Return: length of elements
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t n = 0;
+
+	while (h)
+	{
+		n++;
+		h = h->next;
+	}
+	return (n);
+}
