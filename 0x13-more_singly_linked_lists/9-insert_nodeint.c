a492a8e50d37b08d737b81397f4ca6e9df023760
Osei <williamsabodunrin@gmail.com>
2023-05-02 12:16:30 +0000

    insert node

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -12,16 +12,22 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *ptr = *head;
 	unsigned int a = 0;
 
+	new = malloc(sizeof(listint_t));
+	if (!new)
+		return (NULL);
+	new->n = n;
+	new->next = NULL;
+	if (idx == 0)
+	{
+		new->next = ptr;
+		ptr = new;
+		return (ptr);
+	}
 	while (ptr)
 	{
 		if (a == idx - 1)
 		{
 			swap = ptr->next;
-			new = malloc(sizeof(listint_t));
-			if (!new)
-				return (NULL);
-			new->n = n;
-			new->next = NULL;
 			ptr->next = new;
 			ptr = new;
 			if (swap)
