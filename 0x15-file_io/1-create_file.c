436f3c757adf24f4bdba4e6bda5118896eda2854
osei <williamsabodunrin@gmail.com>
2023-05-08 22:44:57 -0700

    create a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,6 +21,9 @@ int create_file(const char *filename, char *text_content)
 			return (-1);
 		return (1);
 	}
+	fl = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	if (fl == -1)
+		return (-1);
 	write_var = write(fl, text_content, l);
 	if (write_var == -1)
 	{
