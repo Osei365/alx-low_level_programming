bd58cb2a4667404a32851b14b1d6b431d9872c21
osei <williamsabodunrin@gmail.com>
2023-06-23 16:28:07 -0700

    make me win

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 wget -P /tmp https://github.com/Osei365/alx-low_level_programming/raw/master/0x18-dynamic_libraries/jackpotwin.so
-export LD_PRELOAD=/tmp/random.so
+export LD_PRELOAD=/tmp/jackpotwin.so
