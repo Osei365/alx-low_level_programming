0ca4b84ac4d9fe5f180d9167073a91e1b8a58eb3
osei <williamsabodunrin@gmail.com>
2023-06-23 16:09:09 -0700

    call C from Python

---
+++ 0x18-dynamic_libraries/operators.c
@@ -0,0 +1,60 @@
+int add(int a, int b);
+int sub(int a, int b);
+int mul(int a, int b);
+int div(int a, int b);
+int mod(int a, int b);
+
+/**
+ * add - adds two numbers
+ * @a: first number
+ * @b: second number
+ * Return: result
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - subtracts two numbers
+ * @a: first number
+ * @b: second number
+ * Return: result
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - multiplies two numbers
+ * @a: first number
+ * @b: second number
+ * Return: result
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - divides two numbers
+ * @a: first number
+ * @b: second number
+ * Return: result
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - gets modulo of two numbers
+ * @a: first number
+ * @b: second number
+ * Return: result
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
