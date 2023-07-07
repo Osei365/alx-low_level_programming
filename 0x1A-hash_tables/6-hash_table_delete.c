2f9644103d12300b1d36fe33c49c86076f2af107
osei <williamsabodunrin@gmail.com>
2023-07-07 08:55:50 -0700

    delete hash table

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,28 @@
+#include "hash_tables.h"
+/**
+ * hash_table_delete - delete has table
+ * @ht: hash table
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int a;
+	hash_node_t *temp, *node;
+
+	for (a = 0; a < ht->size; a++)
+	{
+		if (ht->array[a])
+		{
+			node = ht->array[a];
+			while(node)
+			{
+				temp = node->next;
+				free(node->key);
+				free(node->value);
+				free(node);
+				node = temp;
+			}
+		}
+	}
+	free(ht->array);
+	free(ht);
+}
