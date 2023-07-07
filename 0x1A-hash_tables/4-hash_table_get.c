47d70cb8f5a3c3f2d449b14401fc2f68d7fccd94
osei <williamsabodunrin@gmail.com>
2023-07-07 07:00:09 -0700

    get value using key from hash table

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,26 @@
+#include "hash_tables.h"
+/**
+ * hash_table_get - gets a value from hash table
+ * @ht: hash table
+ * @key: key in question
+ * Return: value;
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	hash_node_t *node;
+	unsigned long int index;
+
+	index = key_index((const unsigned char *)key, ht->size);
+	if (index >= ht->size)
+		return (NULL);
+	node = ht->array[index];
+	while (node)
+	{
+		if (strcmp(node->key, key) == 0)
+		{
+			return (node->value);
+		}
+		node = node->next;
+	}
+	return (NULL);
+}
