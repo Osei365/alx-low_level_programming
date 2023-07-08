c2cef4276935c00b8d5894dd05c767e4fc58b951
Osei <williamsabodunrin@gmail.com>
2023-07-08 12:38:38 +0000

    create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,7 +8,7 @@
 int create_file(const char *filename, char *text_content)
 {
 	int fl;
-	int l;
+	int l = 0;
 	int write_var;
 
 	if (filename == NULL)
@@ -18,12 +18,9 @@ int create_file(const char *filename, char *text_content)
 		l = strlen(text_content);
 	}
 	fl = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
-	if (fl == -1)
-		return (-1);
 	write_var = write(fl, text_content, l);
-	if (write_var == -1)
+	if (fl == -1 || write_var == -1)
 	{
-		close(fl);
 		return (-1);
 	}
 	close(fl);
