71e90e22a7a107278b0be6cd554bad4c6c3c0c4d
Osei <williamsabodunrin@gmail.com>
2023-10-11 09:40:14 +0000

    interpolation search algo

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -19,7 +19,7 @@ int interpolation_search(int *array, size_t size, int value)
 	high = size - 1;
 	while (low <= high) 
 	{
-		mid = low + (((double)(high - low) / (array[high] - array[low])) * 
+		mid = low + (((double)(high - low) / (array[high] - array[low])) *
 			(value - array[low]));
 		if (mid < size)
 			printf("Value checked array[%lu] = [%i]\n", mid, array[mid]);
