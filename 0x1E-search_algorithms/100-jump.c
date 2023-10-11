ed1d6dbcadf2286fe9595490241406ca43142085
Osei <williamsabodunrin@gmail.com>
2023-10-11 00:12:17 +0000

    jump search

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -29,17 +29,17 @@ int jump_search(int *array, size_t size, int value)
 
 	if (array == NULL || size == 0)
 		return (-1);
-	printf("Value checked array[%lu] = [%i]\n", a, array[a]);
+	printf("Value checked array[%lu] = [%d]\n", a, array[a]);
 	while (array[b] < value && b < size)
 	{
-		printf("Value checked array[%lu] = [%i]\n", b, array[b]);
+		printf("Value checked array[%lu] = [%d]\n", b, array[b]);
 		a = b;
 		b = b + sqrt(size);
 	}
 	printf("Value found between indexes [%lu] and [%lu]\n", a, b);
 	for (i = a; i < size && i <= b; i++)
 	{
-		printf("Value checked array[%lu] = [%i]\n", i, array[i]);
+		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
 		if (array[i] == value)
 			return ((int)i);
 	}
