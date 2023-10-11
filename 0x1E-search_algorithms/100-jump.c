6d98f2cec9e0ea5563651f96e19c84f49073729b
Osei <williamsabodunrin@gmail.com>
2023-10-11 02:31:25 -0700

    jump search

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -35,7 +35,7 @@ int jump_search(int *array, size_t size, int value)
 	{
 		printf("Value checked array[%lu] = [%d]\n", b, array[b]);
 		a = b;
-		b = b + sqrt(size);
+		b = b + i;
 	}
 	printf("Value found between indexes [%lu] and [%lu]\n", a, b);
 	for (; a <= min(b, size - 1); a++)
