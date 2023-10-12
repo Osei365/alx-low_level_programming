c41f206178e04a84761ddaa0f24011be597cc62a
Osei <williamsabodunrin@gmail.com>
2023-10-11 19:08:03 -0700

    jump search linked list

--- 0x1E-search_algorithms/105-jump_list.c
+++ 0x1E-search_algorithms/105-jump_list.c
@@ -25,28 +25,25 @@ size_t min(size_t a, size_t b)
  */
 listint_t *jump_list(listint_t *list, size_t size, int value)
 {
-	size_t a = 0, b;
+	size_t a = 0, b = sqrt(size);
 	listint_t *back, *front;
 
 	if (!list || size == 0)
 		return (NULL);
-	b = sqrt(size);
 	back = front = list;
-	while (b < size)
+	while (front->next && front->index < b)
+		front = front->next;
+	while (front->index < size - 1 && front->n <= value)
 	{
-		while (front->index < b)
-		{
-			front = front->next;
-		}
 		printf("Value checked at index [%lu] = [%i]\n", front->index, front->n);
-		if (front->n > value)
-			break;
 		back = front;
 		a = b;
 		b = b + sqrt(size);
+		while (front->next && front->index < b)
+			front = front->next;
 	}
 	printf("Value found between indexes [%lu] and [%lu]\n",
-		back->index, min(front->index, size - 1));
+		back->index, front->index);
 	for (; a <= min(b, size - 1); a++, back = back->next)
 	{
 		printf("Value checked at index [%lu] = [%i]\n", back->index, back->n);
